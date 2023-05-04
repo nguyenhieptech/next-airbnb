49a9d5f1d42d54718de40c202e4c04f06bc3e5cc
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-05-05 01:22:27 +0700

    Add forwardRef

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { classNames } from '@/app/utils';
-import { ButtonHTMLAttributes } from 'react';
+import React, { forwardRef } from 'react';
 import { IconType } from 'react-icons';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'outline' | 'fill';
   size?: 'sm' | 'md';
   startIcon?: IconType;
@@ -23,32 +23,35 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
  * Naming convention is similar to MUI v5
  * @see https://mui.com/material-ui/react-button/
  */
-export function Button(props: ButtonProps) {
-  const {
-    type = 'button',
-    variant = 'fill',
-    size = 'md',
-    children,
-    startIcon: StartIcon,
-    ...rest
-  } = props;
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  function Button(props, ref) {
+    const {
+      type = 'button',
+      variant = 'fill',
+      size = 'md',
+      children,
+      startIcon: StartIcon,
+      ...rest
+    } = props;
 
-  return (
-    <button
-      className={classNames(
-        'relative w-full rounded-lg text-sm transition hover:opacity-80',
-        'disabled:cursor-not-allowed disabled:opacity-70',
-        'focus:outline focus:outline-2 focus:outline-neutral-700',
-        variant === 'outline'
-          ? 'border border-neutral-700 bg-white text-neutral-700'
-          : 'border-primary bg-primary text-white',
-        size === 'sm' ? 'py-1 font-normal' : 'py-3 font-medium'
-      )}
-      type={type}
-      {...rest}
-    >
-      {StartIcon && <StartIcon className="absolute left-4 top-3" size={24} />}
-      {children}
-    </button>
-  );
-}
+    return (
+      <button
+        className={classNames(
+          'relative w-full rounded-lg text-sm transition hover:opacity-80',
+          'disabled:cursor-not-allowed disabled:opacity-70',
+          'focus:outline focus:outline-2 focus:outline-neutral-700',
+          variant === 'outline'
+            ? 'border border-neutral-700 bg-white text-neutral-700'
+            : 'border-primary bg-primary text-white',
+          size === 'sm' ? 'py-1 font-normal' : 'py-3 font-medium'
+        )}
+        type={type}
+        ref={ref}
+        {...rest}
+      >
+        {StartIcon && <StartIcon className="absolute left-4 top-3" size={24} />}
+        {children}
+      </button>
+    );
+  }
+);
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,46 +1,48 @@
 'use client';
 
 import { classNames } from '@/app/utils';
-import { InputHTMLAttributes } from 'react';
+import React from 'react';
 import { FieldErrors } from 'react-hook-form';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   errors?: FieldErrors | string[] | string;
 }
 
-export function Input(props: InputProps) {
-  const {
-    label,
-    errors,
-    placeholder = 'placeholder',
-    type = 'text',
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  function Input(props, ref) {
+    const {
+      label,
+      errors,
+      placeholder = 'placeholder',
+      type = 'text',
+      ...rest
+    } = props;
 
-    ...rest
-  } = props;
-
-  return (
-    <div className="relative w-full">
-      <input
-        className={classNames(
-          'peer w-full rounded-md border bg-white p-4 pt-6 text-sm text-text-primary placeholder-transparent outline-none transition',
-          'disabled:cursor-not-allowed disabled:opacity-70',
-          errors
-            ? 'border-primary focus:border-2 focus:border-primary'
-            : 'border-neutral-600 focus:border-2 focus:border-text-primary'
-        )}
-        placeholder={placeholder}
-        type={type}
-        {...rest}
-      />
-      <label
-        className={classNames(
-          'absolute left-4.5 top-2 z-10 origin-[0] scale-75 transform text-sm text-text-secondary transition-all',
-          'peer-placeholder-shown:top-5 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:scale-75'
-        )}
-      >
-        {label}
-      </label>
-    </div>
-  );
-}
+    return (
+      <div className="relative w-full">
+        <input
+          className={classNames(
+            'peer w-full rounded-md border bg-white p-4 pt-6 text-sm text-text-primary placeholder-transparent outline-none transition',
+            'disabled:cursor-not-allowed disabled:opacity-70',
+            errors
+              ? 'border-primary focus:border-2 focus:border-primary'
+              : 'border-neutral-600 focus:border-2 focus:border-text-primary'
+          )}
+          placeholder={placeholder}
+          type={type}
+          ref={ref}
+          {...rest}
+        />
+        <label
+          className={classNames(
+            'absolute left-4.5 top-2 z-10 origin-[0] scale-75 transform text-sm text-text-secondary transition-all',
+            'peer-placeholder-shown:top-5 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:scale-75'
+          )}
+        >
+          {label}
+        </label>
+      </div>
+    );
+  }
+);
