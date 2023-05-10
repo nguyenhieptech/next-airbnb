4f039aea63dc3319acb1aa1ccf3f55448f98a127
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-05-10 18:43:33 +0700

    Add toast

--- app/providers/AppProvider.tsx
+++ app/providers/AppProvider.tsx
@@ -4,12 +4,14 @@ import { ReactNode } from 'react';
 import { ModalsProvider } from './ModalsProvider';
 import { StoreProvider } from './StoreProvider';
 import { ThemeProvider } from './ThemeProvider';
+import { ToasterProvider } from './ToastProvider';
 
 export function AppProvider({ children }: { children: ReactNode }) {
   return (
     <StoreProvider>
       <ModalsProvider />
       <ThemeProvider />
+      <ToasterProvider />
       {children}
     </StoreProvider>
   );
