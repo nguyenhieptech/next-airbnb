9e79c68b184a81e4b1f3ff8a7d3cbdf086a51bf8
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-05-16 10:28:33 +0700

    Add URL changes when clicking to category boxes

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useRouter, useSearchParams } from 'next/navigation';
+import qs from 'query-string';
+import { useCallback } from 'react';
 import { IconType } from 'react-icons';
 import { classNames } from '../utils';
 
@@ -7,15 +10,45 @@ interface CategoryBoxProps {
   icon: IconType;
   label: string;
   isSelected?: boolean;
-  onClick?: React.MouseEventHandler<HTMLDivElement> | undefined;
 }
 
 export function CategoryBox(props: CategoryBoxProps) {
-  const { label, isSelected, onClick, icon: Icon } = props;
+  const { label, isSelected, icon: Icon } = props;
+
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    // Toggle/select/deselect category
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: '/',
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, router, params]);
 
   return (
     <div
-      onClick={onClick}
+      onClick={handleClick}
       className={classNames(
         'flex cursor-pointer flex-col items-center justify-center gap-2 border-b-2 p-3 transition hover:text-neutral-800',
         isSelected
