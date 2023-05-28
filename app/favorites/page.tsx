0fff649fd168261745893acbe090a1455b226302
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-05-28 19:24:29 +0700

    Add view favorites feature

---
+++ app/favorites/page.tsx
@@ -0,0 +1,25 @@
+import { getCurrentUser, getFavoriteListings } from '@/app/actions';
+import { ClientOnly, EmptyState } from '@/components';
+import { FavoritesClient } from './FavoritesClient';
+
+export default async function FavoritesPage() {
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <FavoritesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+}
